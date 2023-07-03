dcd60d2998f698a8543e54d0c7db2eb73fa07efc
dev24 <dev24@macmini-dev24.local>
2023-07-03 17:35:28 +0300

    dasd

--- src/js/import/slider.js
+++ src/js/import/slider.js
@@ -8,5 +8,15 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1440: {
+        slidesPerView: 4,
+      },
+  },
 });
